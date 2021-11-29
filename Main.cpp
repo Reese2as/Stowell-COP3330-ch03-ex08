5727c7fb241b9aae539a17e6ddca2c9e70f34c0f
Reese Stowell <Reese2as@gmail.com>
2021-11-28 19:03:23 -0500

    s

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "std_lib_facilities.h"
 
-int main()
+int main() 
 {
-    std::cout << "Hello World" << std::endl;
+    int x;
+    cout << "Please enter a value: ";
+    cin >> x;
+
+    cout << "The value " << x << " is an ";
+    if (x % 2 == 0)
+        cout << "even number.";
+    else
+        cout << "odd number.";
+
+    return 0;
 }
